e78cba7a1cf9913a4658f2aae9317df8d9ff7a62
ImSept <75469418+ImSept@users.noreply.github.com>
2024-06-07 14:15:26 +0800

    Add files via upload

--- load_and_fill.js
+++ load_and_fill.js
@@ -2,7 +2,7 @@
 class DataLoader {
   constructor(request_dictionary, custom_proc = undefined, Fill=true) {
 	this._request_dictionary = request_dictionary;
-	alert('this.xmlhttp = new XMLHttpRequest()');
+
 	
 	this.xmlhttp = new XMLHttpRequest();
 	this.xmlhttp.onload = function() {
@@ -26,12 +26,12 @@ class DataLoader {
 		catch (e) {alert(e)}
 
 	}
-	alert('this.xmlhttp.open');
+
 	this.xmlhttp.open('POST', 'https://coliseum-game.ru:4443');
-	alert('this.xmlhttp.setRequestHeader');
+
 	this.xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	this.xmlhttp.setRequestHeader('Access-Control-Allow-Origin', '*');	
-	alert('this.xmlhttp.send');
+
 	this.xmlhttp.send(JSON.stringify(this._request_dictionary));	
   }
 }
