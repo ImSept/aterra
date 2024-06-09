3816f5cfd8439f5be4b92a7a6da37b6c42e1396e
ImSept <75469418+ImSept@users.noreply.github.com>
2024-06-09 18:11:27 +0800

    Add files via upload

--- load_and_fill.js
+++ load_and_fill.js
@@ -1,7 +1,7 @@
 	// загрузка данных
 	
 
-
+/*
 class DataLoader {
   constructor(request_dictionary, custom_proc = undefined, Fill=true) {
 	$.post( "https://coliseum-game.ru:4443", request_dictionary, function( data ) {
@@ -19,11 +19,11 @@ class DataLoader {
 	}, "json");	
   }
 }
- 	
+ 	*/
 
 
 
-/*
+
 class DataLoader {
   constructor(request_dictionary, custom_proc = undefined, Fill=true) {
 	this._request_dictionary = request_dictionary;
@@ -59,10 +59,23 @@ class DataLoader {
 
 	this.xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
+this.xmlhttp.setRequestHeader("Connection", "keep-alive");
+this.xmlhttp.setRequestHeader("sec-ch-ua", "\"Microsoft Edge\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\", \"Microsoft Edge WebView2\";v=\"125\"");
+this.xmlhttp.setRequestHeader("Accept", "application/json, text/javascript, */*; q=0.01");
+this.xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+this.xmlhttp.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0");
+this.xmlhttp.setRequestHeader("sec-ch-ua-platform", "\"Windows\"");
+this.xmlhttp.setRequestHeader("Sec-Fetch-Site", "cross-site");
+this.xmlhttp.setRequestHeader("Sec-Fetch-Mode", "cors");
+this.xmlhttp.setRequestHeader("Sec-Fetch-Dest", "empty");
+this.xmlhttp.setRequestHeader("Accept-Encoding", "gzip, deflate, br, zstd");
+this.xmlhttp.setRequestHeader("Accept-Language", "ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7");
+
+
 	try {
 		this.xmlhttp.send(JSON.stringify(this._request_dictionary));	
 	}
 	catch (e) {alert(e)}
   }
-}*/
+}
 
